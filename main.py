2740f5530ce6f2ba68b72a515189663c348b5507
laleeks <anavar20@jaumebalmes.net>
2023-01-23 20:23:42 +0100

    Finalizado v2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #En aquest arxiu cada integrant hi posarà la trucada a la seva funció.
 
 #INTEGRANT A - LAIA RODRIGUEZ RAMOS
-import exercici1_A
-import exercici2_A
+#import exercici1_A
+#import exercici2_A
 
 #INTEGRANT B - ALEX NAVARRO RODRIGUEZ
 
+# He comentado su función porque me da error al ejecutar con la mia.
+import exercici1_B as ex1b
+print(ex1b.calcAño(42))
