3dd1ed7be3f1a357d6f95bf297d99b25e893f269
laleeks <115487771+laleeks@users.noreply.github.com>
2023-01-23 20:25:49 +0100

    Delete Alex - B directory

--- Alex - B/exercici1_B.py
+++
@@ -1,6 +0,0 @@
-#Función que introduce la edad y retorna el año de nacimiento.
-
-def calcAño(año):
-    return 2023 - año
-
-print (calcAño(24))
--- Alex - B/exercici2_B.py
+++
@@ -1,5 +0,0 @@
-#Ejercicio de tupla con varios prints de prueba.
-tupla2=(1,4,3,5,6,7)
-print (len(tupla2)) #Longitud de la tupla
-print(3 in tupla2) # Si contiene un valor en la tupla
-print(tupla2.count(4)) #Cuántas veces aparece un valor
