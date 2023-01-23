38694f7f9f734477ee9560b8f4b1570200fba087
LaiaRodri <laia.rodriguez@enti.cat>
2023-01-23 20:32:11 +0100

    Activitats fetes correctament amb el main acabat

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #En aquest arxiu cada integrant hi posarà la trucada a la seva funció.
 
 #INTEGRANT A - LAIA RODRIGUEZ RAMOS
+print("INTEGRANT A: ")
 import exercici1_A
 import exercici2_A
 
 #INTEGRANT B - ALEX NAVARRO RODRIGUEZ
+print("INTEGRANT B: ")
+import exercici1_B as ex1b
+print(ex1b.calcAño(42))
 
